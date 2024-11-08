0dd6a945207f061dbcbee46405ed746934028be1
Jonatan Emil Svendsen <153636466+JohnBBB42@users.noreply.github.com>
2024-11-08 10:13:44 +0100

    Update run_painn_SAM.py

--- scripts/run_painn_SAM.py
+++ scripts/run_painn_SAM.py
@@ -156,16 +156,14 @@ class LitPaiNNModel(L.LightningModule):
             return loss
             
     def _grad_norm(self):
-        device = next(self.parameters()).device
-        norm = torch.norm(
-            torch.stack([
-                p.grad.detach().norm(2).to(device)
-                for p in self.parameters()
-                if p.grad is not None
-            ]),
-            2
-        )
-        return norm
+        grad_norms = []
+        for p in self.parameters():
+            if p.grad is not None:
+                grad_norms.append(p.grad.detach().view(-1))
+        grad_norms = torch.cat(grad_norms)
+        total_norm = torch.norm(grad_norms, p=2)
+        return total_norm
+
         
     def on_before_optimizer_step(self, optimizer):
         # Log the total gradient norm of the model
